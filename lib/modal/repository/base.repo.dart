a1775ddecfbc43f937fbfaf70b947a88ca4c9bd5
Dishank Jindal <dishankjindalitn@gmail.com>
2022-01-03 23:32:53 +0530

    product.repo.dart added

--- lib/modal/repository/base.repo.dart
+++ lib/modal/repository/base.repo.dart
@@ -1,5 +1,8 @@
+import 'package:dio/dio.dart';
+
 const String appUrl = 'https://61d3342ab4c10c001712b860.mockapi.io';
 
 abstract class BaseRepository {
+  Dio? baseDio = Dio();
   Future getResponse();
 }
--- lib/modal/repository/product.repo.dart
+++ lib/modal/repository/product.repo.dart
@@ -1,11 +1,31 @@
+import 'dart:io';
+
 import 'package:applore_assignment_app/modal/repository/base.repo.dart';
+import 'package:applore_assignment_app/utils/utils.dart';
+import 'package:dio/dio.dart';
 
 class ProductRepository implements BaseRepository {
-
   static String path = '/product';
+  @override
+  Dio? baseDio;
 
   @override
-  Future getResponse() {
-    throw UnimplementedError();
+  Future getResponse() async {
+    Response res;
+      res = await baseDio!.get(appUrl + path);
+      Future data = statusResponse(res);
+      return Future.delayed(const Duration(seconds: 2))
+          .then((value) => 'getResponse: Product Repository'.log()).then((value) => data);
+  }
+  
+  Future<Response> statusResponse(Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return response.data;
+      case 404:
+        throw Exception('No Url Found');
+      default:
+        throw Exception('Something unusual happened on the server side');
+    }
   }
 }
